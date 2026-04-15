0e79d9b601981381633c8bd7c04326af05d628cd
Victus44 <rennup4288@gmail.com>
2026-04-15 22:19:51 +0530

    User database connected

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
+const connectDatabase = require("../database/db.js");
+connectDatabase();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const app = require("../src/app");
+const connectDatabase = require("../database/db.js");
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+connectDatabase().then(() => {
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
 });
