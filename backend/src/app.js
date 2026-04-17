d0803670635bd06bd9b942068b5f60709d09aea7
Victus44 <rennup4288@gmail.com>
2026-04-17 18:52:34 +0530

    Created add items module

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const authRoutes = require("../routes/auth.routes.js");
+const itemRoutes = require("../routes/item.routes.js");
 
 const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, "../../frontend")));
+app.use("/uploads", express.static(path.join(__dirname, "../../uploads")));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/items", itemRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
