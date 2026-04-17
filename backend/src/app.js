252709c51c94fce3187ed33fb56927461f454008
Victus44 <rennup4288@gmail.com>
2026-04-17 18:13:40 +0530

    Added user  register, login, and password reset modules with validations

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const authRoutes = require("../routes/auth.routes.js");
 
 const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname, "../../frontend")));
 app.use(express.json());
-
-const connectDatabase = require("../database/db.js");
-connectDatabase();
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
